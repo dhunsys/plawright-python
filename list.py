0cb47aca355d2a80a69994b9883e2023cb707451
msdhunsitemp0025 <shahabuddin0025@gmail.com>
2026-08-08 20:06:52 +0530

    modify list

--- list.py
+++ list.py
@@ -2,3 +2,15 @@ l = [2, "abc", 6.6,2,"abc"]
 print(l)
 print(l[0])
 print(l[1])
+
+
+list=[1,2,3,4,5]
+print("reverse",list[::-1])
+
+list2=[1,4,3,7,5]
+print("sort without modifying original",sorted(list2))
+print(list2)
+
+list3=[1,2,7,0,9,5]
+list3.sort()
+print("sort with modifying original",list3)
